fa797934907acd1f4aeb8f51a0ffed8a2fe1d65f
seyrani <seyranid@gmail.com>
2017-03-13 15:37:41 +0300

    Application Control Coftware

---
+++ App Controller/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace App_Controller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.sagTikMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listView1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(249, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(353, 245);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "No";
+            this.columnHeader1.Width = 33;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 91;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Path";
+            this.columnHeader3.Width = 245;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnGuncelle);
+            this.groupBox1.Controls.Add(this.btnSil);
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.txtPath);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtId);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 135);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Uygulama Ekle";
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(174, 103);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(58, 23);
+            this.btnGuncelle.TabIndex = 2;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(110, 103);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(58, 23);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(50, 103);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(58, 23);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(63, 68);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(169, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Path :";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(63, 16);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(169, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(63, 42);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(169, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "No :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnStart);
+            this.groupBox2.Controls.Add(this.btnStop);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.numUpDown);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(3, 141);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(240, 100);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ayarlar";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(135, 68);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(96, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Başlat";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(18, 68);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(89, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Sonlandır";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(18, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(213, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // numUpDown
+            // 
+            this.numUpDown.Location = new System.Drawing.Point(192, 9);
+            this.numUpDown.Name = "numUpDown";
+            this.numUpDown.Size = new System.Drawing.Size(40, 20);
+            this.numUpDown.TabIndex = 1;
+            this.numUpDown.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Uygulama Seç ve Sonlandır";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(113, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Restart Time :";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Process Controller Running";
+            this.notifyIcon1.Visible = true;
+            
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sagTikMenu});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 48);
+            // 
+            // sagTikMenu
+            // 
+            this.sagTikMenu.Name = "sagTikMenu";
+            this.sagTikMenu.Size = new System.Drawing.Size(152, 22);
+            this.sagTikMenu.Text = "Stop";
+            this.sagTikMenu.Click += new System.EventHandler(this.sagTikMenu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(602, 245);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listView1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Process Controller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown numUpDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sagTikMenu;
+    }
+}
+
---
+++ App Controller/Form1.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace App_Controller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            // simge durum kontrolü
+            this.Resize += SetMinimizeState;
+            // simgeye tıklayınca pencere durumunu tetikle.       
+            notifyIcon1.Click += ToggleMinimizeState;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            xmlOlusturur();
+            listele();
+            listviewRenk();
+        }
+
+
+        // Formu minimize et normale dön
+        private void ToggleMinimizeState(object sender, EventArgs e)
+        {
+            bool isMinimized = this.WindowState == FormWindowState.Minimized;
+            this.WindowState = (isMinimized) ? FormWindowState.Normal : FormWindowState.Minimized;
+        }
+
+        // Simge durumu göster gizle
+        private void SetMinimizeState(object sender, EventArgs e)
+        {
+            try
+            {
+                bool isMinimized = this.WindowState == FormWindowState.Minimized;
+
+                this.ShowInTaskbar = !isMinimized;
+                notifyIcon1.Visible = isMinimized;
+                if (isMinimized) notifyIcon1.ShowBalloonTip(500, "'" + txtName.Text + "'", "Servis kontrolü çalışıyor ...", ToolTipIcon.Info);
+            }
+            catch (Exception ex)
+            {
+                AppendToLog(string.Format("Olay günlüğü [{0}]: [{1}] \r\n", DateTime.Now, ex.Message));
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        //Tex box temizle
+        private void ClearTextBox()
+        {
+            Action<Control.ControlCollection> func = null;
+
+            func = (controls) =>
+            {
+                foreach (Control control in controls)
+                    if (control is TextBox)
+                        (control as TextBox).Clear();
+                    else
+                        func(control.Controls);
+            };
+
+            func(Controls);
+        }
+
+        //Program çalıştığı anda XML oluşturur varsa kontrolünü yapar
+        public void xmlOlusturur()
+        {
+            if (!File.Exists(@"data.xml"))
+            {
+                XmlTextWriter xmlOlustur = new XmlTextWriter(@"data.xml", null);
+                xmlOlustur.WriteStartDocument();
+                xmlOlustur.WriteComment("Process Controller - Seyrani DEMİREL");
+                xmlOlustur.WriteStartElement("Uygulamalar");
+                xmlOlustur.WriteEndDocument();
+                xmlOlustur.Close();
+
+            }
+        }
+
+        //listview data listeler
+        public void listele()
+        {
+            if (File.Exists("data.xml"))
+            {
+                listView1.Items.Clear();
+
+                XmlDocument doc = new XmlDocument();
+
+                doc.Load("data.xml");
+                XmlElement root = doc.DocumentElement;
+                XmlNodeList kayitlar = root.SelectNodes("/Uygulamalar/Uygulama");
+
+                foreach (XmlNode secilen in kayitlar)
+                {
+                    ListViewItem lv = new ListViewItem();
+                    lv.Text = secilen["id"].InnerText;
+                    lv.SubItems.Add(secilen["name"].InnerText);
+                    lv.SubItems.Add(secilen["path"].InnerText);
+                    listView1.Items.Add(lv);
+
+                    if (comboBox1.Items.IndexOf(secilen["name"].InnerText) == -1)
+                    {
+                        comboBox1.Items.Add(secilen["name"].InnerText);
+                    }
+
+
+                }
+            }
+        }
+
+
+        //listview renklendir
+        public void listviewRenk()
+        {
+            ListView listView = this.listView1;
+            int i = 0;
+            Color shaded = Color.FromArgb(240, 240, 240);
+            foreach (ListViewItem item in listView.Items)
+            {
+                if (i++ % 2 == 1)
+                {
+                    item.BackColor = Color.AliceBlue;
+                    item.UseItemStyleForSubItems = true;
+                }
+            }
+        }
+
+        //Listview Kayıt Ekle
+        public void veriKayitEt()
+        {
+            bool varmi = false;
+            int sonID;
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("data.xml");
+            XmlElement root = doc.DocumentElement;
+            XmlNodeList kayitlar = root.SelectNodes("/Uygulamalar/Uygulama");
+            if (kayitlar.Count > 0)
+            {
+                varmi = true;
+            }
+
+            if (varmi == true)
+            {
+                int[] kayittakiSayilar = new int[kayitlar.Count];
+
+                int i = 0;
+                foreach (XmlNode secilen in kayitlar)
+                {
+                    kayittakiSayilar[i] = Convert.ToInt32(secilen["id"].InnerText);
+                    i = i + 1;
+                }
+
+                Array.Sort(kayittakiSayilar);
+                sonID = kayittakiSayilar[kayittakiSayilar.Length - 1];
+                sonID = sonID + 1;
+
+            }
+            else
+            {
+                sonID = 0;
+            }
+
+            if (File.Exists("data.xml"))
+            {
+
+                XmlElement UserElement = doc.CreateElement("Uygulama");
+
+                XmlElement id = doc.CreateElement("id");
+                id.InnerText = sonID.ToString();
+                UserElement.AppendChild(id);
+
+                XmlElement name = doc.CreateElement("name");
+                name.InnerText = txtName.Text;
+                UserElement.AppendChild(name);
+
+                XmlElement path = doc.CreateElement("path");
+                path.InnerText = txtPath.Text;
+                UserElement.AppendChild(path);
+
+                doc.DocumentElement.AppendChild(UserElement);
+
+                XmlTextWriter xmleekle = new XmlTextWriter("data.xml", null);
+                xmleekle.Formatting = Formatting.Indented;
+                doc.WriteContentTo(xmleekle);
+                xmleekle.Close();
+
+            }
+            listele();
+            ClearTextBox();
+
+        }
+
+
+        string id = null;
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem lv = listView1.SelectedItems[0];
+                txtId.Text = lv.SubItems[0].Text;
+                txtName.Text = lv.SubItems[1].Text;
+                txtPath.Text = lv.SubItems[2].Text;
+                id = lv.SubItems[0].Text;
+
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            veriKayitEt();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                XDocument xDoc = XDocument.Load("data.xml");
+                XElement deletedElement = xDoc.Root.Elements().FirstOrDefault(xe => xe.Element("id").Value == id);
+
+                deletedElement.Remove();
+                xDoc.Save("data.xml");
+
+                listele();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Silinecek kayıt yok veya seçilmemiş.");
+            }
+            ClearTextBox();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            XDocument xDoc = XDocument.Load("data.xml");
+
+            XElement currentElement = xDoc.Root.Elements().FirstOrDefault(xe => xe.Element("id").Value == id);
+
+            currentElement.SetElementValue("name", txtName.Text);
+            currentElement.SetElementValue("path", txtPath.Text);
+            xDoc.Save("data.xml");
+
+            listele();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            listView1.Items.Clear();
+
+            XmlDocument doc = new XmlDocument();
+
+            doc.Load("data.xml");
+            XmlElement root = doc.DocumentElement;
+            XmlNodeList kayitlar = root.SelectNodes("/Uygulamalar/Uygulama");
+
+            foreach (XmlNode secilen in kayitlar)
+            {
+
+                if (secilen["name"].InnerText == comboBox1.Text)
+                {
+
+                    ListViewItem lv = new ListViewItem();
+                    lv.Text = secilen["id"].InnerText;
+                    lv.SubItems.Add(secilen["name"].InnerText);
+                    lv.SubItems.Add(secilen["path"].InnerText);
+                    listView1.Items.Add(lv);
+
+                }
+
+            }
+        }
+
+        //loglama
+        string logFilename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments), @"C:\Process Controller\LogsImageServerController.log");
+
+        private void AppendToLog(string info)
+        {
+            try
+            {
+                if (File.Exists(logFilename))
+                {
+                    FileInfo fi = new FileInfo(logFilename);
+                    if (fi.Length > 1024 * 1024)
+                    {
+                        File.WriteAllLines(logFilename, File.ReadAllLines(logFilename).Skip(500));
+                    }
+                }
+                File.AppendAllText(logFilename, info + "\n");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        //uygulama başlat
+        public void ProcessThreadAsync()
+        {
+
+
+            try
+            {
+
+                var path = txtPath.Text;
+                Process process = new Process();
+
+                while (true)
+                {
+
+                    FileInfo fi = new FileInfo(path);
+
+                    ProcessStartInfo info = new ProcessStartInfo(path);
+                    info.WorkingDirectory = fi.DirectoryName;
+
+                    process.StartInfo = info;
+
+                    process.Start();
+
+                    Task.Factory.StartNew(() =>
+           {
+               int count = Convert.ToInt32(Math.Round(numUpDown.Value, 0));
+               Thread.Sleep(count * 60 * 1000);
+               process.Kill();
+
+           });
+                    process.WaitForExit();
+
+                    // process.TotalProcessorTime cpu kullanım değeri 
+                    AppendToLog(string.Format("Olay günlüğü Start/Stop [{0}]: [{1}] \r\n", DateTime.Now, process.StartTime));
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                AppendToLog(string.Format("Olay günlüğü [{0}]: [{1}] \r\n", DateTime.Now, ex.Message));
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        Thread baslat;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            baslat = new Thread(new ThreadStart(ProcessThreadAsync));
+            baslat.Start();
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Process.Start("taskkill", "/F /IM " + comboBox1.SelectedItem.ToString());
+            }
+            catch (Exception ex)
+            {
+                AppendToLog(string.Format("Olay günlüğü [{0}]: [{1}] \r\n", DateTime.Now, ex.Message));
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void sagTikMenu_Click(object sender, EventArgs e)
+        {
+            baslat.Abort();
+        }
+    }
+
+
+
+
+
+
+}
